fa807ba2299c5d19587d8838854c115d42a6b47e
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-10-31 14:39:14 +0300

    initial commit

---
+++ src/main/java/pa1_wordnet/Demo.java
@@ -0,0 +1,13 @@
+package pa1_wordnet;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by ilyarudyak on 10/31/15.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        StdOut.println("just check if libs are working");
+    }
+}
