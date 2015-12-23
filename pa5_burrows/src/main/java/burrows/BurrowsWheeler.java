d5c6892dd049e7ca28cdbfdbd97b0f1166bd4009
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-12-23 16:29:16 +0300

    done decode()

--- pa5_burrows/src/main/java/burrows/BurrowsWheeler.java
+++ pa5_burrows/src/main/java/burrows/BurrowsWheeler.java
@@ -3,6 +3,9 @@ package burrows;
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
 
 /**
  * Created by ilyarudyak on 12/22/15.
@@ -42,6 +45,38 @@ public class BurrowsWheeler {
     // standard input and writing to standard output
     public static void decode() {
 
+        int first = BinaryStdIn.readInt();
+        char[] t = BinaryStdIn.readString().toCharArray();
+        char[] t1 = Arrays.copyOf(t, t.length);
+        Arrays.sort(t1);
+        int[] next = next(t);
+
+        int temp = first;
+        for (int i = 0; i < t.length; i++) {
+            BinaryStdOut.write(t1[temp]);
+            temp = next[temp];
+        }
+
+        BinaryStdOut.close();
+    }
+
+    private static int[] next(char[] t) {
+
+        char[] tcopy = Arrays.copyOf(t, t.length);
+        char[] t1 = Arrays.copyOf(t, t.length);
+        Arrays.sort(t1);
+        int[] next = new int[tcopy.length];
+
+        for (int i = 0; i < tcopy.length; i++) {
+            for (int j = 0; j < tcopy.length; j++) {
+                if (t1[i] == tcopy[j]) {
+                    next[i] = j;
+                    tcopy[j] = '\0';
+                    break;
+                }
+            }
+        }
+        return next;
     }
 
     // if args[0] is '-', apply Burrows-Wheeler encoding
@@ -52,3 +87,24 @@ public class BurrowsWheeler {
         else throw new IllegalArgumentException("Illegal command line argument");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
